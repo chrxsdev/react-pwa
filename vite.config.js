ca79c3badc199897e1c4f41768ea91f04decef09
Cristhian Martinez <cristhian.martinezlara@gmail.com>
2025-11-28 20:52:02 -0600

    adding sw saving cache and network strategy

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     VitePWA({
       strategies: 'injectManifest',
       srcDir: 'src',
-      filename: 'sw-template.js',
+      filename: 'sw.js',
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
